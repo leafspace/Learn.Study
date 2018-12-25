f583f8df2e04be1ed9d7f04b1e6db54df2c1e1fe
leafspace <leafspace_zhang@163.com>
2018-12-25 21:45:46 +0800

    add Leetcode/#0844.比较含退格的字符串.cpp Signed-off-by: leafspace <leafspace_zhang@163.com>

---
+++ Leetcode/#0844.比较含退格的字符串.cpp
@@ -0,0 +1,72 @@
+/*
+给定 S 和 T 两个字符串，当它们分别被输入到空白的文本编辑器后，判断二者是否相等，并返回结果。 # 代表退格字符。
+
+ 
+
+示例 1：
+
+输入：S = "ab#c", T = "ad#c"
+输出：true
+解释：S 和 T 都会变成 “ac”。
+示例 2：
+
+输入：S = "ab##", T = "c#d#"
+输出：true
+解释：S 和 T 都会变成 “”。
+示例 3：
+
+输入：S = "a##c", T = "#a#c"
+输出：true
+解释：S 和 T 都会变成 “c”。
+示例 4：
+
+输入：S = "a#c", T = "b"
+输出：false
+解释：S 会变成 “c”，但 T 仍然是 “b”。
+ 
+
+提示：
+
+1 <= S.length <= 200
+1 <= T.length <= 200
+S 和 T 只含有小写字母以及字符 '#'。
+*/
+
+class Solution {
+public:
+    bool compareStack(stack<char> S, stack<char> T) {
+        while(!S.empty() && !T.empty()) {
+            if (S.top() != T.top()) {
+                cout << "Stack is diffrent ! (" << S.top() << "," << T.top()  << ")" << endl;
+                return false;
+            }
+            S.pop();
+            T.pop();
+        }
+        
+        if (!S.empty() || !T.empty()) {
+            cout << "Have stack not empty !" << endl;
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    stack<char> string2Stack(string pStr) {
+        stack<char> pStack;
+        for (int i = 0; i < pStr.length(); ++i) {
+            if (pStr[i] == '#') {
+                if (!pStack.empty()) {
+                    pStack.pop();
+                }
+                continue;
+            }
+            pStack.push(pStr[i]);
+        }
+        return pStack;
+    }
+    
+    bool backspaceCompare(string S, string T) {
+        return this->compareStack(this->string2Stack(S), this->string2Stack(T));
+    }
+};
