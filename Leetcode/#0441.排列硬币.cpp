d18a9ad88afa7784a739c4c11eaebbd4492e1a57
leafspace <leafspace_zhang@163.com>
2020-02-21 09:36:05 +0800

    Create #0441.排列硬币.cpp

---
+++ Leetcode/#0441.排列硬币.cpp
@@ -0,0 +1,57 @@
+/*
+你总共有 n 枚硬币，你需要将它们摆成一个阶梯形状，第 k 行就必须正好有 k 枚硬币。
+
+给定一个数字 n，找出可形成完整阶梯行的总行数。
+
+n 是一个非负整数，并且在32位有符号整型的范围内。
+
+示例 1:
+
+n = 5
+
+硬币可排列成以下几行:
+¤
+¤ ¤
+¤ ¤
+
+因为第三行不完整，所以返回2.
+示例 2:
+
+n = 8
+
+硬币可排列成以下几行:
+¤
+¤ ¤
+¤ ¤ ¤
+¤ ¤
+
+因为第四行不完整，所以返回3.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/arranging-coins
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+    double getCoins(double k)
+    {
+        return (k * k + k) / 2;
+    }
+
+    int arrangeCoins(int n) {
+        int k = sqrt(n);
+        do
+        {
+            if (getCoins(k) <= n && n < getCoins(k + 1))
+            {
+                return k;
+            }
+            else
+            {
+                k++;
+            }
+        } while(true);
+        return 0;
+    }
+};
