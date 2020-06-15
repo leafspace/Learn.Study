6acd327876a1332dccf345de2d92f0069422d3a9
leafspace <leafspace_zhang@163.com>
2020-06-15 09:07:36 +0800

    Create #0014.最长公共前缀.cpp

---
+++ Leetcode/#0014.最长公共前缀.cpp
@@ -0,0 +1,60 @@
+/*
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1:
+
+输入: ["flower","flow","flight"]
+输出: "fl"
+示例 2:
+
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+说明:
+
+所有输入只包含小写字母 a-z 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-common-prefix
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string retStr = "";
+        if (strs.size() == 0)
+        {
+            return retStr;
+        }
+        
+        for (int i = 0, j = 0; true; ++i)
+        {
+            char charaterIndex = strs[0][i];
+            for (j = 0; j < strs.size(); ++j)
+            {
+                if (i == strs[j].size())
+                {
+                    break;
+                }
+
+                if (strs[j][i] != charaterIndex)
+                {
+                    break;
+                }
+            }
+
+            if (j == strs.size())
+            {
+                retStr = retStr + charaterIndex;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return retStr;
+    }
+};
