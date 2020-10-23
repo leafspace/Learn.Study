72fcec4349b997016695ad601da74c5b83c12f1e
leafspace <leafspace_zhang@163.com>
2020-10-23 19:53:28 +0800

    【代码更新】增加electron示例

---
+++ Electron/ElectronTest/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+//  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
